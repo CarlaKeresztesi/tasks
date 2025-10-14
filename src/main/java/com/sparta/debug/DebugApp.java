0d8105330a7621ca992b09d2e854b382719b54db
carla <carlakeresztesi@gmail.com>
2025-10-14 12:54:14 +0100

    DebugApp task resolved

--- src/main/java/com/sparta/debug/DebugApp.java
+++ src/main/java/com/sparta/debug/DebugApp.java
@@ -2,6 +2,19 @@ package com.sparta.debug;
 
 public class DebugApp {
     public static void main(String[] args) {
-        System.out.println("Hello from DebugApp!");
+        var name = "Josh Crabtree";
+        String occupation = "trainee";
+        String location = "London";
+        var activityLevel = "moderate";
+
+        System.out.println("My name is " + name);
+        System.out.printf("I am a %s in %s%n", occupation, location);
+
+        int age = 21;
+        System.out.println("I am " + age + " years of age, with a "+ activityLevel + " activity level");
+
+        // or formatted:
+        System.out.printf("I am %d years of age, with a %s activity level.%n", age, activityLevel);
+        
     }
 }
