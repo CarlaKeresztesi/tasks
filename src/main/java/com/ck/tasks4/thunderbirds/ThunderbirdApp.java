7c4ad7b29729261e3e2c14a9191faca550e5be7f
carla <carlakeresztesi@gmail.com>
2025-10-16 21:53:01 +0100

    ThunderbirdsApp with scanner and switch st

--- src/main/java/com/ck/tasks4/thunderbirds/ThunderbirdApp.java
+++ src/main/java/com/ck/tasks4/thunderbirds/ThunderbirdApp.java
@@ -10,8 +10,24 @@ public class ThunderbirdApp {
         System.out.print("Enter a number between 1 and 4: ");
         int number = scanner.nextInt();
 
+        switch (number) {
+            case 1:
+                System.out.println("Thunderbird 1 pilot is Scott Tracy");
+                break;
+            case 2:
+                System.out.println("Thunderbird 2 pilot is Virgil Tracy");
+                break;
+            case 3:
+                System.out.println("Thunderbird 3 pilot is Alan Tracy");
+                break;
+            case 4:
+                System.out.println("Thunderbird 4 pilot is Gordon Tracy");
+                break;
+            default:
+                System.out.println("Have you never watched Thunderbirds!");
+        }
 
-
+        scanner.close();
 
     }
 }
