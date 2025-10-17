7293f3d9ebee9dfeefa3a4e4689adddceb784b16
carla <carlakeresztesi@gmail.com>
2025-10-17 21:09:05 +0100

    Finalised ProCalculatorApp using scanner, enhanced switch, conditionals and exceptional/abnormal control flow

--- src/main/java/com/ck/tasks5/procalculator/ProCalculatorApp.java
+++ src/main/java/com/ck/tasks5/procalculator/ProCalculatorApp.java
@@ -8,31 +8,68 @@ public class ProCalculatorApp {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Please enter two positive integer values and then an operator from the following: +, -, /, or *.");
+        System.out.println("Please enter two positive integer values and then an operator from the following: +, -, /, or *. ");
 
         try {
             int num1 = scanner.nextInt();
             int num2 = scanner.nextInt();
             char operator = scanner.next().charAt(0);//takes the next input from the scanner, grabs the 1st char of it
-                                                            //and stores it as the operator
+            //and stores it as the operator
             if (num1 <= 0 || num2 <= 0) {
                 System.out.println("Error: Both number should be positive integers.");
                 return;
             }
 
-            int result = 0;
+            int result;
             boolean valid = true;
 
             switch (operator) {
-                case '+':
-                    result = num1 + num2;
-                    break;
+                case '+' -> result = num1 + num2;
+                case '-' -> result = num1 - num2;
+                case '*' -> result = num1 * num2;
+                case '/' -> {
+                    if (num2 != 0) {
+                        result = num1 / num2;
+                    } else {
+                        System.out.println("Error. Division by 0 is not allowed.");
+                        valid = false;
+                        result = 0;
+                    }
+                }
+                default -> {
+                    System.out.println("Error. Invalid operator. Please use +, -, * or /.");
+                    valid = false;
+                    result = 0;
+                }
+            }
+            ;
 
+//            switch (operator) {
+//                case '+':
+//                    result = num1 + num2;
+//                    break;
+//                case '-':
+//                    result = num1 - num2;
+//                    break;
+//                case '*':
+//                    result = num1 * num2;
+//                    break;
+//                case '/':
+//                    result = num1 / num2;
+//                    break;
+//            }
+
+
+            if (valid) {
+                System.out.println("Your sum is " + num1 + " " + operator + " " + num2 + " and the answer is " + result);
             }
 
+        } catch (InputMismatchException e) {
+        System.out.println("Error: Please enter positive integers followed by an operator.");
+        } catch (Exception e) {
+            System.out.println("An error occurred : " + e.getMessage());
+        } finally {
+            scanner.close();
         }
-
-        catch (InputMismatchException e) {
-        System.out.println("Error: Please enter a positive integer value.");}
     }
 }
